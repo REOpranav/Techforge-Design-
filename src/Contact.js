1ca958d86684396eec17f3ef374baaab8ddd929f
REOpranav <pranavavadivel01@gmail.com>
2023-08-02 03:43:10 +0530

    Creating components and adding method

---
+++ src/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        
+        <h1>follow & contact</h1>
+
+        <div className='h2'>
+            <h2>MGPT</h2>
+            <h2>BOOKING</h2>
+            <h2>PRESS</h2>
+            <h2>MGPT INFO</h2>
+        </div>
+
+ <div className='p'>
+            <div>
+                <p>pranav</p>
+                <p>+917812817300</p>
+                <p>pranav@mail.com</p>
+            </div>
+
+            <div>
+                <p>vadivel</p>
+                <p>+917812817300</p>
+                <p>vadivel@mail.com</p>
+            </div>
+        
+            <div>
+                <p>iangar</p>
+                <p>+917812817300</p>
+                <p>iangar@mail.com</p>
+            </div>
+        
+            <div>
+                <p>sundhar</p>
+                <p>+917812817300</p>
+                <p>sundhar@mail.com</p>
+            </div>
+     </div>
+
+     <div className='img'>
+        <img src="https://cdn.pixabay.com/photo/2015/07/13/07/41/icons-842893_640.png" 
+             className='face-book'
+        />
+
+        <img src="https://cdn.pixabay.com/photo/2016/08/01/21/00/icon-1562136_640.png"
+              className='instagram'
+        />
+        <img src="https://cdn.pixabay.com/photo/2021/06/14/04/44/spotify-6334914_640.png"
+              className='spotify' />
+
+        <img src="https://cdn.pixabay.com/photo/2016/11/18/11/16/social-1834013_640.png" 
+            className='twitter'/> 
+
+         <img src="https://cdn.pixabay.com/photo/2016/09/05/12/38/you-tube-1646474_640.png" 
+            className='you-tube' />   
+     </div>
+ </div>
+  )
+}
+
+export default Contact
